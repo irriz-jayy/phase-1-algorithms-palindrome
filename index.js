d1c96b420778f454b831401a6894d68c6bb58152
irriz-jayy <jayfelix789@gmail.com>
2022-12-07 15:45:00 +0300

    Added explanation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const word = ["racecar", "abba", "a", "ab", "robot"];
 
 function isPalindrome(word) {
   var len = word.length;
-  for (var i = 0; i < len / 2; i++) {
+  for (var i = 0; i < len; i++) {
     if (word[i] !== word[len - 1 - i]) {
       return false;
     }
@@ -15,14 +15,13 @@ function isPalindrome(word) {
   create function isPalindrome with argument of word
 
   iterate through an array of word
-  returns true if word is a palindrome
-  return false if word is not a palindrome
-  push onto result
-  return results.
+  checks the length of the word and takes the last index and subtracts one and checks if they match with the new index
+  then returns true
+  if not retuurns false
 
 
 /*
-  Add written explanation of your solution here
+  The function checks the index of the word. Then takes the lenght of the word subtracts by obe and takes the index and subtracts it. If the letter that is there in the cuurent new index is strictly similar to the expected it passes true. If it isnt it passes false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -35,6 +34,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 }
 
 module.exports = isPalindrome;
