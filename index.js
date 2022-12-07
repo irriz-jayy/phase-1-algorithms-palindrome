328f8e4aecd295a0e10b55c9f13c928abc36b780
irriz-jayy <jayfelix789@gmail.com>
2022-12-07 15:36:12 +0300

    Added pseudocode

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+const word = ["racecar", "abba", "a", "ab", "robot"];
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  var len = word.length;
+  for (var i = 0; i < len / 2; i++) {
+    if (word[i] !== word[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
-*/
+  create function isPalindrome with argument of word
+
+  iterate through an array of word
+  returns true if word is a palindrome
+  return false if word is not a palindrome
+  push onto result
+  return results.
+
 
 /*
   Add written explanation of your solution here
